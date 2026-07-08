fc00bdc66dfe047101a12d7b594a965ebd2dcbd6
Tra Nguyen <147636502+Trasonoisy@users.noreply.github.com>
2026-07-08 16:42:02 +0700

    Fix app message to make CI pass

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Automated Multi-Environment Deployment Pipeline Demo",
+    message: "Automated Multi-Environment Deployment Pipeline",
     environment: process.env.NODE_ENV || "development"
   });
 });
